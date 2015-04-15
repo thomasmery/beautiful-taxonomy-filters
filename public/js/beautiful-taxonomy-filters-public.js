04f1bf573455dc59b78c51ac2bae3d8bfde49da3
Thomas Mery <thomas.mery@gmail.com>
2015-04-16 00:21:49 +0200

    added styling to selected options

--- public/js/beautiful-taxonomy-filters-public.js
+++ public/js/beautiful-taxonomy-filters-public.js
@@ -6,8 +6,33 @@
 	$(document).ready(function(){
 		$('.beautiful-taxonomy-filters-select').select2({
 			allowClear: true,
-			minimumResultsForSearch: -1
+			minimumResultsForSearch: -1,
+			formatSelection: function(object, container) {
+
+				// reset
+				container.parent().removeClass();
+				
+				//re-add default classes for this element
+				container.parent().addClass("select2-choice");
+
+				// make the closed box take the selected option color
+				container.parent().addClass(object.css);
+
+				return object.text;
+
+			}
 		});
+
+		$('.beautiful-taxonomy-filters-select').on('select2-clearing', function(e) { 
+			
+			var $selected = $(this).parent().find('.select2-choice')
+			$selected.removeClass();
+				
+			//re-add default classes for this element
+			$selected.addClass("select2-choice");
+
+		});
+
 	});
 
 
